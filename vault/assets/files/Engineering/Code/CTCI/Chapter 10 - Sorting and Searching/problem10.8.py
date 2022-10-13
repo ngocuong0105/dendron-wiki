a531ce103fc5966ea2f4059822c81600a0ad5c66
Ngo Cuong <ngo.cuong@abv.bg>
2022-10-13 22:25:53 +0100

    delete big files
    Former-commit-id: 128c0bec55e53e13026a21ae1edc5ea5a0c9753e [formerly 128c0bec55e53e13026a21ae1edc5ea5a0c9753e [formerly 128c0bec55e53e13026a21ae1edc5ea5a0c9753e [formerly 77723e7ec0cc1ee2a5bbf8b63e47e7e0f838f2fb]]] [formerly 5c4ac8d1f7fddf252df4d70f577fbb1174297e27] [formerly 5706ca665fd032fc6e2a399196a2ca6bacb436c8 [formerly 5706ca665fd032fc6e2a399196a2ca6bacb436c8 [formerly 5706ca665fd032fc6e2a399196a2ca6bacb436c8 [formerly 8937701948973125cac81522a2a350f29d00a9b1]]]] [formerly 5706ca665fd032fc6e2a399196a2ca6bacb436c8 [formerly 5706ca665fd032fc6e2a399196a2ca6bacb436c8 [formerly 5706ca665fd032fc6e2a399196a2ca6bacb436c8 [formerly 8937701948973125cac81522a2a350f29d00a9b1]]] [formerly e8666d4a6cb96efd4e1c118684587b7da003b47d]] [formerly 5706ca665fd032fc6e2a399196a2ca6bacb436c8 [formerly 5706ca665fd032fc6e2a399196a2ca6bacb436c8 [formerly 5706ca665fd032fc6e2a399196a2ca6bacb436c8 [formerly 8937701948973125cac81522a2a350f29d00a9b1]]] [formerly e8666d4a6cb96efd4e1c118684587b7da003b47d] [formerly a523700a6c67bff47cf6ca0b755c1a674f929813 [formerly a523700a6c67bff47cf6ca0b755c1a674f929813 [formerly a523700a6c67bff47cf6ca0b755c1a674f929813 [formerly 4f66a79eade2c905e46edb2a0170c3fa5dba9766]]]]]
    Former-commit-id: 5437e1453f3e027e6458afce868f8a0727503797 [formerly 9cbc71f4defa125f1d07d9c9951bece807298049] [formerly d58fef8bbb122e5a4c11ac9684ff5e6a029f7b6a [formerly ac46ba3d0ad76652c8f37730338cf856ce1c37b0]] [formerly 582e4ada281199f2eb8cee0ae9b8e475ca042215 [formerly 582e4ada281199f2eb8cee0ae9b8e475ca042215 [formerly 6c208ecb9baf354eb728c2aa815ed78f55a69974]]] [formerly 5c31c2bba739b300ec949d8d23d724a832ca4c6f] [formerly a523700a6c67bff47cf6ca0b755c1a674f929813]
    Former-commit-id: 5437e1453f3e027e6458afce868f8a0727503797 [formerly 9cbc71f4defa125f1d07d9c9951bece807298049] [formerly d58fef8bbb122e5a4c11ac9684ff5e6a029f7b6a [formerly ac46ba3d0ad76652c8f37730338cf856ce1c37b0]] [formerly 582e4ada281199f2eb8cee0ae9b8e475ca042215 [formerly 582e4ada281199f2eb8cee0ae9b8e475ca042215 [formerly 6c208ecb9baf354eb728c2aa815ed78f55a69974]]] [formerly 5c31c2bba739b300ec949d8d23d724a832ca4c6f]
    Former-commit-id: 5437e1453f3e027e6458afce868f8a0727503797 [formerly 9cbc71f4defa125f1d07d9c9951bece807298049] [formerly d58fef8bbb122e5a4c11ac9684ff5e6a029f7b6a [formerly ac46ba3d0ad76652c8f37730338cf856ce1c37b0]] [formerly 582e4ada281199f2eb8cee0ae9b8e475ca042215 [formerly 582e4ada281199f2eb8cee0ae9b8e475ca042215 [formerly 6c208ecb9baf354eb728c2aa815ed78f55a69974]]]
    Former-commit-id: b44ecb1230e592102600d7e70d7a36ce0d5cecbd
    Former-commit-id: 7ae68421a20bf4f49d6f6b61ab0b4a077eb09109 [formerly 43742d8cf99832ca009047c53b0b1debb0280fed] [formerly ca43776d6a87b1a56175bcfa1cf9b83139488425 [formerly ba1e53f3c0baa845aa4a8f9a1be81a6dd895dff9]]
    Former-commit-id: 2d6a57f210d92b3d74ba6ee0bac418d68e5dc558 [formerly 8cdc049d04ec15658121c462baf4290a651216e7]
    Former-commit-id: c262dbdddccd30b16dfa414e15d7f8509a46df21

--- vault/assets/files/Engineering/Code/CTCI/Chapter 10 - Sorting and Searching/problem10.8.py
+++
@@ -1,8 +0,0 @@
-# Find duplicate in linear time constant space
-def findDuplicate(nums: list[int]) -> int:
-    n = len(nums)
-    for num in nums:
-        nums[num] += n
-    for i in range(len(nums)):
-        if nums[i] // n > 1:
-            return i
